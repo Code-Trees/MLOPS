cc0cd24d238e7d8406a7f297c2c47a3362b9544d
Pruthiraj Jayasingh <data.pruthiraj@gmail.com>
2024-05-20 14:36:59 +0530

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -7,7 +7,7 @@ pipeline = load('regression.joblib')
 # Streamlit app
 st.title("Sentiment Analysis App")
 
-st.write("This app predicts the sentiment of a sentence as positive or negative.")
+st.write("This app predicts the sentiment of a sentence as Neutral, Negative, Positive, or Irrelevant.")
 
 # Input fields for the name and the sentence
 name = st.text_input("Enter your name:")
@@ -18,10 +18,9 @@ if st.button("Predict Sentiment"):
     if sentence:
         # Predict the sentiment
         prediction = pipeline.predict([sentence])[0]
-        sentiment = "positive" if prediction == 'positive' else "negative"
         
         # Display the result
-        st.write(f"Hello {name}, the sentiment of the given sentence is **{sentiment}**.")
+        st.write(f"Hello {name}, the sentiment of the given sentence is **{prediction}**.")
     else:
         st.write("Please enter a sentence to analyze.")
 
